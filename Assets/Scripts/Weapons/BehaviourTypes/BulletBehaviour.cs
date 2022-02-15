e5ee950b66fa3e2b9aeb0b7af29a524fd03b8575
Max <maxpham99@gmail.com>
2022-02-14 22:20:04 -0500

    Reduced IHitBehaviour minimally: only start

--- Assets/Scripts/Weapons/BehaviourTypes/BulletBehaviour.cs
+++ Assets/Scripts/Weapons/BehaviourTypes/BulletBehaviour.cs
@@ -4,25 +4,8 @@ using UnityEngine;
 
 public class BulletBehaviour : IHitBehaviour
 {
-    float _damage;
-    public float Damage 
-    { 
-        get => _damage; 
-        set => _damage = value; 
-    }
-    float _bulletSpeed = 1f;
-    public float BulletSpeed {
-        get => _bulletSpeed;
-        set => _bulletSpeed = value;
-    }
-
     public void startBehaviour(Vector3 position, Vector3 direction, int damage, float bulletSpeed)
     {
         BulletMono.create(position, direction, damage, bulletSpeed); 
-    }
-
-    public void onHit()
-    {
-        Debug.Log("Peashooter Hit something");
     }
 }
--- Assets/Scripts/Weapons/IHitBehaviour.cs
+++ Assets/Scripts/Weapons/IHitBehaviour.cs
@@ -4,7 +4,5 @@ using UnityEngine;
 
 public interface IHitBehaviour 
 {
-    float Damage { get; set;}
     void startBehaviour(Vector3 position, Vector3 direction, int damage, float bulletSpeed);
-    void onHit();
 }
