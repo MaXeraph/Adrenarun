df4adbac0fc1e6095363c8b35b87a992ea044bdb
Max <33969759+MaXeraph@users.noreply.github.com>
2022-02-17 14:51:55 -0500

    Basic Weapon behaviours (#4)
    * Abstracted guns + bullets
    - Basic Peashooter
    
    * Rename folder
    
    * Formatting Bullet interface
    
    * Added UML diagram from excalidraw
    
    * Use float for FireRate + Damage
    
    * Add CODEOWNERS on PRs
    
    * checkpoint
    
    * Renaming files + create modifier variables
    
    * checkpoint:
    - Add Bullet Prefab
    - Add BulletMono script to handle create
    - wip
    
    * Attack detection on all Button events
    
    * Reduced IHitBehaviour minimally: only start
    
    * Add Enemy Spawner, Speed Manager to Scene
    
    * Changed IHitBehaviour to take in statsModifiers
    - Basic Attack logic
    - BulletMono triggers when collide
    
    * Changes:
    - decoupled attack direction
    - moved hitModifiers to behaviors
    - removed self-destruct bullets
    
    * Remove artifacts
    
    * Add todo for enemySpawn
    
    * Added Stats and Stats dependent trigger logic
    
    * Zero out StatsModifiers + Add Stats to Enemy

--- Assets/Scripts/Player/PlayerControls.cs
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -6,10 +6,9 @@ public class PlayerControls : MonoBehaviour
 {
     // Start is called before the first frame update
     private GameObject _player;
-    private GameObject _camera;
-    private GameObject _gun;
-
+    private Weapon _weapon;
     private Vector3 _velocity;
+    private Camera _camera;
 
     bool isGrounded;
 
@@ -17,8 +16,8 @@ public class PlayerControls : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         _player = GameObject.FindWithTag("Player");
-        _camera = GameObject.FindWithTag("MainCamera");
-        // _gun = GameObject.FindWithTag("Gun");
+        _camera = Camera.main;
+        _weapon = GetComponent<Weapon>();
     }
 
     // Update is called once per frame
@@ -28,7 +27,7 @@ public class PlayerControls : MonoBehaviour
 
         if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
         {
-            Movement.RotatePlayer(_player, _camera);
+            Movement.RotatePlayer(_player, _camera.gameObject);
         }
 
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
@@ -46,9 +45,15 @@ public class PlayerControls : MonoBehaviour
             Movement.playerDash(_player);
         }
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetButton("Fire1"))
         {
-            ShootControl.Shoot(_gun);
+            Vector3 position = _camera.transform.forward + _camera.transform.position;
+            Vector3 direction = _camera.transform.forward;
+            _weapon.Attack(position, direction);
+        }
+        if (Input.GetButtonDown("Reload"))
+        {
+            _weapon.Reload();
         }
 
 
--- Assets/Scripts/SpeedManager.cs
+++ Assets/Scripts/SpeedManager.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class SpeedManager : MonoBehaviour
 {
-    public static float coreSpeed = 1f; // 1:1 with health percentage
+    public static float coreSpeed = 5f; // 1:1 with health percentage
     public static float playerMovementScaling;
     public static float bulletSpeedScaling;
     public static float enemySpawnScaling;
-    static AudioSource music;
 
     void Start() {
-        music = GameObject.Find("AudioManager").GetComponent<AudioSource>();
         playerMovementScaling = coreSpeed; //TODO: determine player movement ratio based on speed
         bulletSpeedScaling = coreSpeed; //TODO: determine bullet speed ratio based on speed
         enemySpawnScaling = coreSpeed; //TODO: determine spawn rate based on speed
@@ -22,9 +20,9 @@ public class SpeedManager : MonoBehaviour
     }
 
     static void updateGameObjectSpeed(){
-        playerMovementScaling = coreSpeed / 1;
-        bulletSpeedScaling = coreSpeed / 1;
-        enemySpawnScaling = coreSpeed / 1;
+        playerMovementScaling = coreSpeed;
+        bulletSpeedScaling = coreSpeed;
+        enemySpawnScaling = coreSpeed;
     }
 
 }
