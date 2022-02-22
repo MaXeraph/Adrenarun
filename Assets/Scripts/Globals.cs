f2b7552387c13e88052b17abfbc90888582ec66e
Sirkular <sirkular.sirkle@hotmail.com>
2022-02-22 17:32:34 -0500

    Refactor EnemyFactory for dynamic creation
    * Renamed Enemy to Turret
    * Added EnemyType enum and enemyPrefabNames to Globals to facilitate multiple enemy types
    * Refactored EnemyFactory's CreateEnemy to dynamically create the enemy based on the corresponding EnemyInfo

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Globals
+public static class Globals
 {
 	public static Vector3 DirectTargeting(Transform from, Transform to) {
 		Vector3 direction = (to.position - from.position).normalized;
 		return direction;
 	}
+
+	public static Dictionary<EnemyType, string> enemyPrefabNames = new Dictionary<EnemyType, string>()
+	{
+		{ EnemyType.TURRET, "Turret" }
+	};
+}
+
+public enum EnemyType
+{
+	TURRET
 }
 
 public enum EntityType {
