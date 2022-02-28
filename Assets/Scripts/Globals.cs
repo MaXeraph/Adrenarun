1bd1b20a4cb2f0383b149607b76bddef01101bd0
Max <33969759+MaXeraph@users.noreply.github.com>
2022-02-27 23:53:26 -0500

    Implement Healing Varient of Turret (#49)
    * CreateEnemy takes in Varient type as well
    
    * Removed Clamp restriction with setHealth
    
    * Add Healing Variant

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -4,40 +4,51 @@ using UnityEngine;
 
 public static class Globals
 {
-	public static Dictionary<PowerUpType, StatPowerUp> StatPowerUpDictionary = new Dictionary<PowerUpType, StatPowerUp>
-	{{PowerUpType.DAMAGE, new StatPowerUp(5f)},
-	{PowerUpType.FIRERATE, new StatPowerUp(-0.04f)},
-	{PowerUpType.RELOADSPD, new StatPowerUp(1f)},
-	{PowerUpType.CLIPSIZE, new StatPowerUp(2f)},
-	{PowerUpType.ADRENALIN, new StatPowerUp(0.15f)}
-	};
+    public static Dictionary<PowerUpType, StatPowerUp> StatPowerUpDictionary = new Dictionary<PowerUpType, StatPowerUp>
+    {{PowerUpType.DAMAGE, new StatPowerUp(5f)},
+    {PowerUpType.FIRERATE, new StatPowerUp(-0.04f)},
+    {PowerUpType.RELOADSPD, new StatPowerUp(1f)},
+    {PowerUpType.CLIPSIZE, new StatPowerUp(2f)},
+    {PowerUpType.ADRENALIN, new StatPowerUp(0.15f)}
+    };
 
-	public static Vector3 DirectTargeting(Transform from, Transform to) {
-		Vector3 direction = (to.position - from.position).normalized;
-		return direction;
-	}
+    public static Vector3 DirectTargeting(Transform from, Transform to)
+    {
+        Vector3 direction = (to.position - from.position).normalized;
+        return direction;
+    }
 
-	public static Dictionary<EnemyType, string> enemyPrefabNames = new Dictionary<EnemyType, string>()
-	{
-		{ EnemyType.TURRET, "Turret" }
-	};
+    public static Dictionary<EnemyType, string> enemyPrefabNames = new Dictionary<EnemyType, string>()
+    {
+        { EnemyType.TURRET, "Turret" },
+        { EnemyType.HEALER, "Healer" }
+    };
 }
 
 public enum EnemyType
 {
-	TURRET
+    TURRET,
+    HEALER
 }
 
-public enum EntityType {
-	PLAYER,
-	ENEMY
+public enum EnemyVariantType
+{
+    NONE,
+    HEALER
 }
 
-public enum PowerUpType{
-	NONE,
-	DAMAGE,
-	FIRERATE,
-	RELOADSPD,
-	CLIPSIZE,
-	ADRENALIN
+public enum EntityType
+{
+    PLAYER,
+    ENEMY
+}
+
+public enum PowerUpType
+{
+    NONE,
+    DAMAGE,
+    FIRERATE,
+    RELOADSPD,
+    CLIPSIZE,
+    ADRENALIN
 }
