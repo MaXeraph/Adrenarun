4eb287cec44696f95021cfcec7de8519f90e6859
Sirkular <sirkular.sirkle@hotmail.com>
2022-03-09 23:34:45 -0500

    Fix syntax bugs, add tag to bullet

--- Assets/Scripts/UI/BulletDetection.cs
+++ Assets/Scripts/UI/BulletDetection.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,7 @@ using UnityEngine.UI;
 
 public class BulletDetection : MonoBehaviour
 {
-	private static planeXZ = new Vector(0, 1, 0);
+	private static Vector3 planeXZ = new Vector3(0, 1, 0);
 	private Collider[] collisions = new Collider[36];
 	private Collider self;
 
@@ -16,23 +17,23 @@ public class BulletDetection : MonoBehaviour
 	public Vector3[] bulletDirections = new Vector3[36];
 	public int numDir = 0;
 	
-	Start()
+	void Start()
 	{
 		// Can replace with the object this is attached to
 		self = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>();
 	}
 
-	Update()
+	void Update()
 	{
 		// Player capsule has height 2, radius 0.5
 		// We want bullets to be detected as if collider with height 3, radius 1
 		// point0 is the center of the top hemisphere, point1 is the center of the bottom hemisphere
-		Vector3 point0 = target.transform.position;
-		point0.y += 2 / 2 - 0.5;
-		Vector3 point1 = target.transform.position;
-		point1.y -= 2 / 2 - 0.5;
+		Vector3 point0 = self.transform.position;
+		point0.y += 2f / 2f - 0.5f;
+		Vector3 point1 = self.transform.position;
+		point1.y -= 2f / 2f - 0.5f;
 		int numCollisions = Physics.OverlapCapsuleNonAlloc(point0, point1, radius, collisions, layerMask);
-		Array.Clear(bulletDirections, null, bulletDirections.length);
+		Array.Clear(bulletDirections, 0, bulletDirections.Length);
 		numDir = 0;
 		for (int i = 0; i < numCollisions; i++)
 		{
