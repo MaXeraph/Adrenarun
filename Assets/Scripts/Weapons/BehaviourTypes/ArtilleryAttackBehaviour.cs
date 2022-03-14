c8806ae2393bc40edc7a044e889289e1ffb84e6c
Max <maxpham99@gmail.com>
2022-03-14 12:56:22 -0400

    Format with spaces

--- Assets/Scripts/Weapons/BehaviourTypes/ArtilleryAttackBehaviour.cs
+++ Assets/Scripts/Weapons/BehaviourTypes/ArtilleryAttackBehaviour.cs
@@ -7,53 +7,53 @@ using UnityEngine;
  */
 public class ArtilleryAttackBehaviour : BulletAttackBehaviour
 {
-	public int thermiteDurability;
-	public float thermiteDamageCooldown;
+    public int thermiteDurability;
+    public float thermiteDamageCooldown;
 
 
-	public ArtilleryAttackBehaviour(EntityType owner, float damage = 5f, float bulletSpeed = 10f, int durability = 10, float damageCooldown = 0.25f)
-		: base(owner, damage, bulletSpeed)
-	{
-		thermiteDurability = durability;
-		thermiteDamageCooldown = damageCooldown;
-	}
+    public ArtilleryAttackBehaviour(EntityType owner, float damage = 5f, float bulletSpeed = 10f, int durability = 10, float damageCooldown = 0.25f)
+        : base(owner, damage, bulletSpeed)
+    {
+        thermiteDurability = durability;
+        thermiteDamageCooldown = damageCooldown;
+    }
 
-	public override void initiateAttack(Vector3 position, Vector3 direction)
-	{
-		ArtilleryMono.Create(this, position, direction);
-	}
+    public override void initiateAttack(Vector3 position, Vector3 direction)
+    {
+        ArtilleryMono.Create(this, position, direction);
+    }
 
-	public override void onHit(BulletMono bm, GameObject target)
-	{
-		if (target.tag != "Enemy" && target.tag != "Detector" && (target.tag == "Player" || target.GetComponent<BulletMono>() == null)) // if not another bullet...
-		{
-			Vector3 position = bm.gameObject.GetComponent<Transform>().position;
-			Collider[] explosionHits = Physics.OverlapSphere(position, 4f); // 4f = scale of ThermitePool/2
-			for (int i = 0; i < explosionHits.Length; i++)
-			{
-				Collider collider = explosionHits[i];
-				if (collider.gameObject.tag == "Player")
-				{
-					Stats statsComponent = collider.gameObject.GetComponent<Stats>();
-					statsComponent.currentHealth -= _damage / 2; // Deal half damage on explosion.
-					ArtilleryMono.Destroy(bm.gameObject);
-					break;
-				}
-			}
+    public override void onHit(BulletMono bm, GameObject target)
+    {
+        if (target.tag != "Enemy" && target.tag != "Detector" && (target.tag == "Player" || target.GetComponent<BulletMono>() == null)) // if not another bullet...
+        {
+            Vector3 position = bm.gameObject.GetComponent<Transform>().position;
+            Collider[] explosionHits = Physics.OverlapSphere(position, 4f); // 4f = scale of ThermitePool/2
+            for (int i = 0; i < explosionHits.Length; i++)
+            {
+                Collider collider = explosionHits[i];
+                if (collider.gameObject.tag == "Player")
+                {
+                    Stats statsComponent = collider.gameObject.GetComponent<Stats>();
+                    statsComponent.currentHealth -= _damage / 2; // Deal half damage on explosion.
+                    ArtilleryMono.Destroy(bm.gameObject);
+                    break;
+                }
+            }
 
-			RaycastHit[] hitInfo = Physics.RaycastAll(position + new Vector3(0, 5, 0), new Vector3(0, -1, 0));
-			for (int i = 0; i < hitInfo.Length; i++)
-			{
-				if (hitInfo[i].collider.gameObject.tag == "Platform")
-				{
-					GameObject thermitePool = GameObject.Instantiate(Resources.Load("ThermitePool")) as GameObject;
-					// Spawn the pool on the ground.
-					thermitePool.GetComponent<Transform>().position = hitInfo[i].point;
-					thermitePool.GetComponent<ThermitePoolMono>().Initialize(this);
-					ArtilleryMono.Destroy(bm.gameObject);
-					return;
-				}
-			}
-		}
-	}
+            RaycastHit[] hitInfo = Physics.RaycastAll(position + new Vector3(0, 5, 0), new Vector3(0, -1, 0));
+            for (int i = 0; i < hitInfo.Length; i++)
+            {
+                if (hitInfo[i].collider.gameObject.tag == "Platform")
+                {
+                    GameObject thermitePool = GameObject.Instantiate(Resources.Load("ThermitePool")) as GameObject;
+                    // Spawn the pool on the ground.
+                    thermitePool.GetComponent<Transform>().position = hitInfo[i].point;
+                    thermitePool.GetComponent<ThermitePoolMono>().Initialize(this);
+                    ArtilleryMono.Destroy(bm.gameObject);
+                    return;
+                }
+            }
+        }
+    }
 }
--- Assets/Scripts/Weapons/BehaviourTypes/BulletMono.cs
+++ Assets/Scripts/Weapons/BehaviourTypes/BulletMono.cs
@@ -5,38 +5,38 @@ using UnityEngine;
 // Behaviour for Bullet game objects
 public class BulletMono : MonoBehaviour
 {
-	private BulletAttackBehaviour _attackBehaviour;
+    private BulletAttackBehaviour _attackBehaviour;
 
-	public static GameObject Create(BulletAttackBehaviour attackBehaviour, Vector3 position, Vector3 direction)
-	{
-		GameObject newBullet = ObjectPool.Create("Bullet");
+    public static GameObject Create(BulletAttackBehaviour attackBehaviour, Vector3 position, Vector3 direction)
+    {
+        GameObject newBullet = ObjectPool.Create("Bullet");
 
-		newBullet.GetComponent<BulletMono>()._attackBehaviour = attackBehaviour;
+        newBullet.GetComponent<BulletMono>()._attackBehaviour = attackBehaviour;
 
-		newBullet.transform.position = position;
-		newBullet.transform.forward = direction;
-		if (attackBehaviour.Owner == EntityType.ENEMY) newBullet.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
+        newBullet.transform.position = position;
+        newBullet.transform.forward = direction;
+        if (attackBehaviour.Owner == EntityType.ENEMY) newBullet.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
 
-		return newBullet;
-	}
+        return newBullet;
+    }
 
-	public static void Destroy(GameObject bullet)
-	{
-		// Reset to default values
-		bullet.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.white);
-		bullet.GetComponent<TrailRenderer>().Clear();
-		ObjectPool.Destroy("Bullet", bullet);
-	}
+    public static void Destroy(GameObject bullet)
+    {
+        // Reset to default values
+        bullet.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.white);
+        bullet.GetComponent<TrailRenderer>().Clear();
+        ObjectPool.Destroy("Bullet", bullet);
+    }
 
-	void Update()
-	{
-		transform.position += transform.forward * _attackBehaviour._bulletSpeed * Time.deltaTime * SpeedManager.bulletSpeedScaling;
-	}
+    void Update()
+    {
+        transform.position += transform.forward * _attackBehaviour._bulletSpeed * Time.deltaTime * SpeedManager.bulletSpeedScaling;
+    }
 
-	void OnTriggerEnter(Collider c)
-	{
-		if (c.gameObject.tag == "Detector") { if (_attackBehaviour.Owner == EntityType.ENEMY) { CrosshairUI.addIndicator(c.transform.position); } return; }
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "Detector") { if (_attackBehaviour.Owner == EntityType.ENEMY) { CrosshairUI.addIndicator(c.transform.position); } return; }
 
-		_attackBehaviour.onHit(this, c.gameObject);
-	}
+        _attackBehaviour.onHit(this, c.gameObject);
+    }
 }
