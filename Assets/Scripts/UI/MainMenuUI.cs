a275c34bcc457c79f84e1b4e3b41222ef77056f2
ikeia <55465420+ikeia@users.noreply.github.com>
2022-03-25 16:57:29 -0400

    Main menu visuals
    - Bouncing title
    - Scale text on hover
    - Background

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -2,19 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using TMPro;
+using DG.Tweening;
 
 public class MainMenuUI : MonoBehaviour
 {
     public Button start;
     public Button cont;
     public Button quit;
+	public static RectTransform title;
+	private static RectTransform _start;
+	private static RectTransform _cont;
+	private static RectTransform _quit;
 
-    void Start()
+	public static float scale_amount = 1.25f;
+
+	public static Sequence anim;
+
+	Transform background;
+
+	void Start()
     {
         start.onClick.AddListener(() => ButtonPress("start"));
         cont.onClick.AddListener(() => ButtonPress("continue"));
         quit.onClick.AddListener(() => ButtonPress("quit"));
+
+		title = transform.GetChild(4).GetComponent<RectTransform>();
+		background = transform.GetChild(5);
+		_start = start.transform.GetChild(0).GetComponent<RectTransform>();
+		_cont = cont.transform.GetChild(0).GetComponent<RectTransform>();
+		_quit = quit.transform.GetChild(0).GetComponent<RectTransform>();
+		title.DOJumpAnchorPos(title.anchoredPosition, 10f, 1, 1.5f).SetLoops(-1).SetEase(Ease.InOutQuad);
+	}
+
+	void Update()
+    {
+		background.position -= new Vector3(0, 0, 0.5f);
     }
 
     void ButtonPress(string action)
@@ -33,4 +56,46 @@ public class MainMenuUI : MonoBehaviour
                 break;
         }
     }
+
+	public static void OnMouseOver(string item)
+	{
+		DOTween.SmoothRewind(anim);
+		anim = DOTween.Sequence().SetEase(Ease.InOutQuad);
+		switch (item)
+		{
+			case "title":
+				anim.Insert(0, title.DOScale(scale_amount, 0.5f));
+				break;
+			case "start":
+				anim.Insert(0, _start.DOScale(scale_amount, 0.5f));
+				break;
+			case "continue":
+				anim.Insert(0, _cont.DOScale(scale_amount, 0.5f));
+				break;
+			case "quit":
+				anim.Insert(0, _quit.DOScale(scale_amount, 0.5f));
+				break;
+		}
+	}
+
+	public static void OnMouseExit(string item)
+	{
+		DOTween.SmoothRewind(anim);
+		anim = DOTween.Sequence().SetEase(Ease.InOutQuad);
+		switch (item)
+		{
+			case "title":
+				anim.Insert(0, title.DOScale(1, 0.5f));
+				break;
+			case "start":
+				anim.Insert(0, _start.DOScale(1, 0.5f));
+				break;
+			case "continue":
+				anim.Insert(0, _cont.DOScale(1, 0.5f));
+				break;
+			case "quit":
+				anim.Insert(0, _quit.DOScale(1, 0.5f));
+				break;
+		}
+	}
 }
